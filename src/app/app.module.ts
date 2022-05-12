56d2110dd4b9099853b5f64c95698897aa0a3d25
Asim-Coding <muhammadasim19955@gmail.com>
2022-05-13 02:42:44 +0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,22 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { FormsModule } from '@angular/forms';
-
-// import { AppComponent } from './app.component';
-// import { HelloComponent } from './hello.component';
-// import { HeroesComponent } from './heroes/heroes.component';
-
-// @NgModule({
-//   imports:      [ BrowserModule, FormsModule ],
-//   declarations: [ AppComponent, HelloComponent,HeroesComponent ],
-//   bootstrap:    [ AppComponent ]
-// })
-// export class AppModule { }
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-
